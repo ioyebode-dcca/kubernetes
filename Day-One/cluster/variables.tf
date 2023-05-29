1b9aa42b79209f1e139f49394cc3d6d354a7c7e4
oyebode23 <adeoti13@gmail.com>
2023-05-29 18:18:53 +0000

    Setup EKS workflow

--- Day-One/cluster/variables.tf
+++ Day-One/cluster/variables.tf
@@ -1,11 +1,11 @@
 variable "pubsub1" {
   type = string
-  default = "subnet-051c49a4d8434be66"
+  default = "subnet-0bd163902e4165d24"
 }
 
 variable "pubsub2" {
   type = string
-  default = "subnet-02273f25c912d46ee"
+  default = "subnet-0fcac7bf0f5271fc5"
 }
 
 variable "eksIAMRole" {
